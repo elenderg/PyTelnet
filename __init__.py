55d2d6c425be6b38f06a062c0568aabf37b638df
SilvanKohler <edu.silvan.kohler@gmail.com>
2019-05-17 10:41:40 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="telnet",
+    version="1.0.0",
+    author="Silvan Kohler",
+    author_email="edu.silvan.kohler@gmail.com",
+    description="A small package for sending and recieving commands per tcp",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
